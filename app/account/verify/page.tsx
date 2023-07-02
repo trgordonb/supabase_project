fe5db56e05a1055168781ab2f88e69280f1845ee
trgordonb <gordoncheng@outlook.com>
2023-07-02 16:21:27 +0800

    change email verify route

--- app/account/verify/page.tsx
+++ app/account/verify/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-import { Session } from '@supabase/auth-helpers-nextjs'
 
-export default function EmailVerify({ session }: { session: Session | null }) {
+export default function EmailVerify() {
 
     return (
         <div className='flex h-screen w-screen items-center justify-center text-gray-500'>
